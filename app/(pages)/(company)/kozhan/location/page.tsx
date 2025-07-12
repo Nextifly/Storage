d01b840e52ab947971d8aa74b288dc53b7f2dc32
JupiterIT5 <geno74@bk.ru>
2025-07-12 19:57:46 +0500

    update size

--- app/(pages)/(company)/kozhan/location/page.tsx
+++ app/(pages)/(company)/kozhan/location/page.tsx
@@ -12,14 +12,14 @@ const Page = () => {
 				<h2 className='text-white font-bold'>Месторождения</h2>
 			</div>
 			<section className='flex flex-row gap-6 w-full justify-around items-center m-8'>
-				<div className='bg-white py-6 px-9 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] w-[362px]  gap-[30px] flex flex-col'>
+				<div className='bg-[#e2eaf5] py-6 px-9 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] w-[362px]  gap-[30px] flex flex-col'>
 					<LocationTitle title='ПСиПН Морское' />
 					<LocationStorage img={Storage5} />
 					<LocationStorage img={Storage6} />
 					<LocationStorage img={Storage7} />
 					<LocationStorage img={Storage8} />
 				</div>
-				<div className='bg-white py-6 px-9 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] w-[362px]  gap-[30px] flex flex-col'>
+				<div className='bg-[#e2eaf5] py-6 px-9 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] w-[362px]  gap-[30px] flex flex-col'>
 					<LocationTitle title='ПСиСН Каратон' />
 					<LocationStorage img={Storage5} />
 					<LocationStorage img={Storage6} />
--- app/(pages)/(company)/maten_petroleum/location/page.tsx
+++ app/(pages)/(company)/maten_petroleum/location/page.tsx
@@ -13,18 +13,18 @@ const Page = () => {
 				<h2 className='text-white font-bold'>Месторождения</h2>
 			</div>
 			<section className='flex flex-row gap-6 w-full justify-start items-start m-4'>
-				<div className='bg-white py-6 px-9 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] w-[362px]  gap-[30px] flex flex-col'>
+				<div className='bg-[#e2eaf5] py-6 px-9 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] w-[362px]  gap-[30px] flex flex-col'>
 					<LocationTitle title='Кара-Арна' />
 					<LocationStorage img={Storage5} />
 					<LocationStorage img={Storage6} />
 					<LocationStorage img={Storage7} />
 				</div>
-				<div className='bg-white py-6 px-9 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] w-[362px]  gap-[30px] flex flex-col'>
+				<div className='bg-[#e2eaf5] py-6 px-9 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] w-[362px]  gap-[30px] flex flex-col'>
 					<LocationTitle title=' НПС Каратон' />
 					<LocationStorage img={Storage5} />
 					<LocationStorage img={Storage6} />
 				</div>
-				<div className='bg-white py-6 px-9 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] w-[362px]  gap-[30px] flex flex-col'>
+				<div className='bg-[#e2eaf5] py-6 px-9 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] w-[362px]  gap-[30px] flex flex-col'>
 					<LocationTitle title='ПСиСН Каратон' />
 					<LocationStorage img={Storage5} />
 				</div>
